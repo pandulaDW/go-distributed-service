b301e685c4d2a0d31115df37404291ea8aa44aad
Pandula Weerasooriya <pandula.official@gmail.com>
2021-12-20 17:59:03 +0530

    Added http server and initial log code

--- main.go
+++ main.go
@@ -2,8 +2,42 @@ package main
 
 import (
 	"fmt"
+	"github.com/pandulaDW/go-distributed-service/server"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
-	fmt.Println("server started...")
+	httpServer := server.NewHTTPServer(":8080")
+	isServerRunning := make(chan bool)
+
+	sig := make(chan os.Signal)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM, syscall.SIGKILL)
+
+	go func() {
+		<-sig
+		err := httpServer.Close()
+		if err != nil {
+			fmt.Println("error closing the server: ", err)
+		}
+	}()
+
+	go func() {
+		defer func() {
+			isServerRunning <- false
+		}()
+		fmt.Println("server listening to requests at port 8080...")
+		err := httpServer.ListenAndServe()
+		if err != http.ErrServerClosed && err != nil {
+			fmt.Println("server unexpectedly closed: ", err)
+		} else {
+			fmt.Println("closed the server successfully...")
+		}
+	}()
+
+	<-isServerRunning
+	fmt.Println("exiting the process...")
+	os.Exit(1)
 }
