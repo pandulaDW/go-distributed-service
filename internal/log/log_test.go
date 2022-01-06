2bfdebc4e8c34ef0f93e974067568579aa6de72b
Pandula Weerasooriya <pandula.official@gmail.com>
2022-01-06 11:29:31 +0530

    Added log testing

---
+++ internal/log/log_test.go
@@ -0,0 +1,116 @@
+package log
+
+import (
+	"fmt"
+	api "github.com/pandulaDW/go-distributed-service/api/v1"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestLog(t *testing.T) {
+	for scenario, fn := range map[string]func(t *testing.T, log *Log){
+		"append and read a record succeeds": testAppendRead,
+		"offset out of range error":         testOutOfRangeErr,
+		"initialize with existing segments": testInitExisting,
+		"reader":                            testReader,
+		"truncate":                          testTruncate,
+	} {
+		t.Run(scenario, func(t *testing.T) {
+			dir, err := ioutil.TempDir("", "store-test")
+			require.NoError(t, err)
+			defer func(path string) {
+				_ = os.RemoveAll(path)
+			}(dir)
+
+			c := Config{}
+			c.Segment.MaxStoreBytes = 32
+			log, err := NewLog(dir, c)
+			require.NoError(t, err)
+
+			fn(t, log)
+		})
+	}
+}
+
+func testAppendRead(t *testing.T, log *Log) {
+	record := &api.Record{Value: []byte("hello world")}
+	off, err := log.Append(record)
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+
+	read, err := log.Read(off)
+	require.NoError(t, err)
+	require.Equal(t, record.Value, read.Value)
+}
+
+func testOutOfRangeErr(t *testing.T, log *Log) {
+	read, err := log.Read(1)
+	require.EqualError(t, err, "offset out of range: 1")
+	require.Nil(t, read)
+}
+
+func testInitExisting(t *testing.T, log *Log) {
+	record := &api.Record{Value: []byte("hello world")}
+	for i := 0; i < 3; i++ {
+		_, err := log.Append(record)
+		require.NoError(t, err)
+	}
+	require.NoError(t, log.Close())
+
+	off, err := log.LowestOffset()
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+
+	off, err = log.HighestOffset()
+	require.NoError(t, err)
+	require.Equal(t, uint64(2), off)
+
+	newLog, err := NewLog(log.Dir, log.Config)
+	require.NoError(t, err)
+
+	off, err = newLog.LowestOffset()
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+
+	off, err = newLog.HighestOffset()
+	require.NoError(t, err)
+	require.Equal(t, uint64(2), off)
+}
+
+func testReader(t *testing.T, log *Log) {
+	record := &api.Record{Value: []byte("hello world")}
+
+	off, err := log.Append(record)
+	require.NoError(t, err)
+	require.Equal(t, uint64(0), off)
+
+	reader := log.Reader()
+	b, err := ioutil.ReadAll(reader)
+	require.NoError(t, err)
+
+	read := &api.Record{}
+	err = proto.Unmarshal(b[lenWidth:], read)
+	require.NoError(t, err)
+	require.Equal(t, record.Value, read.Value)
+}
+
+func testTruncate(t *testing.T, log *Log) {
+	for i := 0; i < 3; i++ {
+		record := &api.Record{Value: []byte(fmt.Sprintf("hello world %d", i))}
+		_, err := log.Append(record)
+		require.NoError(t, err)
+	}
+
+	err := log.Truncate(1)
+	require.NoError(t, err)
+
+	_, err = log.Read(0)
+	require.Error(t, err)
+
+	read, err := log.Read(2)
+	require.NoError(t, err)
+	require.Equal(t, []byte("hello world 2"), read.Value)
+}
